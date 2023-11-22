4164fd5e3695ab02a5884d825db82b294601d58b
youssef-salih <d.youssefsalih@gmail.com>
2023-11-22 13:33:06 +0100

    maj fixed

--- src/components/payAVisit/PayAVisit.jsx
+++ src/components/payAVisit/PayAVisit.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { phone, email, location } from "../../assets/icons";
-import PayElement from "./payElement";
+import PayElement from "./PayElement";
 const PayAVisit = () => {
   return (
     <div className="bg-myorange p-2 rounded-2xl">
