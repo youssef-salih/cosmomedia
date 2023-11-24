25dc453d39e7bead132791a470f8452ca8377a47
youssef-salih <d.youssefsalih@gmail.com>
2023-11-24 16:38:45 +0100

    project added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="container mx-auto ">
+      <div className="bg-[#282828] z-50 relative rounded-lg px-16 py-8 flex">
+        <div className="">
+          <h1 className="text-[#F64E2A] text-8xl">our project</h1>
+          <p className="font-montserat w-full">
+            Explore Real-Life Examples of Our Proven Digital Marketing Success
+            through Our Projects
+          </p>
+        </div>
+        <div className="flex justify-end w-3/4 items-center">
+          <Link to="#" className="underline text-gray-200 text-5xl">View All</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
