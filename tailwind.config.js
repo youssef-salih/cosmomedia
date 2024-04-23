9888828bbe37f9ea5e5108ce455b3fed6583546b
youssef-salih <d.youssefsalih@gmail.com>
2024-04-23 13:58:47 +0100

    background fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
         kaki: "#CF4325",
       },
       backgroundImage: {
-        texture: "url('./src/assets/image/main.svg')",
+        texture: "url('/background.svg')",
         "main-gradient": "linear-gradient(to left, #00ECBE, #00B7C3)",
         "linear-pattern":
           "linear-gradient(to bottom, rgb(0 236 190 / 70%), rgb(0 236 190 / 70%) 47%, transparent, transparent 47%, transparent, transparent 55%, rgb(0 236 190 / 70%) 50%, rgb(0 236 190 / 70%))",
